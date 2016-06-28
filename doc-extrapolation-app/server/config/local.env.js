8bd7022a23fe48ab01af7df9ff8722a29a20ebd6
Jenni Bradstreet <jennic15@gmail.com>
2016-06-28 09:47:46 -0700

    Add oauth and keep keys hidden.

--- doc-extrapolation-app/server/config/local.env.js
+++ doc-extrapolation-app/server/config/local.env.js
@@ -9,8 +9,8 @@ module.exports = {
   DOMAIN: 'http://localhost:9000',
   SESSION_SECRET: 'docextrapolation-secret',
 
-  GOOGLE_ID: 'app-id',
-  GOOGLE_SECRET: 'secret',
+  GOOGLE_ID: process.env.GOOGLE_ID,
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET,
 
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
