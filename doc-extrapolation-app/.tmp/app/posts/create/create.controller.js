925df01496910eb61b72da857c1267b9b6ea9a45
Jenni Bradstreet <jennic15@gmail.com>
2016-06-29 15:47:15 -0700

    Edit schema to save post data.

--- doc-extrapolation-app/.tmp/app/posts/create/create.controller.js
+++ doc-extrapolation-app/.tmp/app/posts/create/create.controller.js
@@ -24,7 +24,9 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
           this.$http.post('/api/posts', {
             // below are values I want to pass into the db
             title: this.post.title,
-            synopsis: this.post.synopsis
+            synopsis: this.post.synopsis,
+            image_1: this.post.image_1,
+            caption_1: this.post.caption_1
           });
           this.post.title = '';
         }
--- doc-extrapolation-app/client/app/posts/create/create.controller.js
+++ doc-extrapolation-app/client/app/posts/create/create.controller.js
@@ -14,7 +14,9 @@ class CreateComponent {
       this.$http.post('/api/posts', {
         // below are values I want to pass into the db
         title: this.post.title,
-        synopsis: this.post.synopsis
+        synopsis: this.post.synopsis,
+        image_1: this.post.image_1,
+        caption_1: this.post.caption_1
       });
       this.post.title = '';
     }
